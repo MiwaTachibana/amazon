0e09def0400a4c2fda6778e8c058c016b01f6e66
Miwa Tachibana <tachibam@gmail.com>
2016-02-03 20:43:51 -0800

    sets up bcrypt

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var port = process.env.PORT || 3000
 var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/amazon';
+var UserRouter = require('./models/user.js');
+
 
 mongoose.connect(mongoUri);
 
@@ -16,6 +18,12 @@ app.use(bodyParser.urlencoded({extended: false} ) );
 
 app.get('/', function(req, res) {
 	res.json( {messsage: "Hello!"} );
+})
+
+app.post('/create-user', function(req, res) {
+	var user = new UserRouter();
+
+	user.profile.name = 
 })
 
 app.listen(port);
